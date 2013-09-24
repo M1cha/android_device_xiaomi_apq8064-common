f5988f7c0cdb56fc72b18329890a52b9c81f1824
M1cha <sigmaepsilon92@gmail.com>
2013-09-24 12:36:40 +0000

    add dualboot_init for dualboot and TDB(TrueDualBoot) support
    Change-Id: Ib9139a132bddc94f22d780db62bdb66cf0745dbe
    
    Conflicts:
    
    	device.mk
    
    Conflicts:
    	device.mk
    	init.aries.rc

--- apq8064.mk
+++ apq8064.mk
@@ -147,6 +147,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
+# Dualboot
+PRODUCT_PACKAGES += \
+    dualboot_init
+
 PRODUCT_PACKAGES += \
     wcnss_service \
     crda \
